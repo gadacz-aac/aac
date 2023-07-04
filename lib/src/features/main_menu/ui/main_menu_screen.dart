3062ef6b38aeed77eb99f33176f4fed41f4be041
WiktorGórecki <wiktor.gorecki@o2.pl>
2023-07-04 13:13:19 +0200

    Moved settings to it's file

--- lib/src/features/main_menu/ui/main_menu_screen.dart
+++ lib/src/features/main_menu/ui/main_menu_screen.dart
@@ -1,4 +1,5 @@
 import 'package:aac/src/features/boards/board_screen.dart';
+import 'package:aac/src/features/settings/ui/settings_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
@@ -62,20 +63,6 @@ class MainMenuScreen extends StatelessWidget {
             ),
           ],
         ),
-      ),
-    );
-  }
-}
-
-class SettingsScreen extends StatelessWidget {
-  const SettingsScreen({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: const Text('Settings')),
-      body: const Center(
-        child: Text('Settings'),
       ),
     );
   }
