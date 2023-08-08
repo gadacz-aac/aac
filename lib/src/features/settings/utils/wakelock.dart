5b352befdcdceb5af5316b09248008a684c721a5
WiktorGórecki <wiktor.gorecki@o2.pl>
2023-08-08 14:48:22 +0200

    Updated wakelock.dart Signed-off-by: WiktorGórecki <wiktor.gorecki@o2.pl>

--- lib/src/features/settings/utils/wakelock.dart
+++ lib/src/features/settings/utils/wakelock.dart
@@ -1,9 +1,14 @@
 import 'package:wakelock/wakelock.dart';
+import 'package:aac/src/features/settings/settings_manager.dart';
 
-void startWakelock() {
+void startWakelock(ref) async {
+  final isEnabled =
+      await ref.watch(settingsManagerProvider).getValue("wakelock");
+  if (isEnabled == null || !isEnabled) return;
   Wakelock.enable();
 }
 
 void stopWakelock() {
   Wakelock.disable();
+  return;
 }
