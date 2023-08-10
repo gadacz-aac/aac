fee4bcda2a53053489940410550ce9ee7b1ef476
peters04 <piotreksmolinski04@gmail.com>
2023-08-10 23:49:21 +0200

    no longer showing spinner

--- lib/src/features/boards/ui/pin_symbol_action.dart
+++ lib/src/features/boards/ui/pin_symbol_action.dart
@@ -1,37 +1,15 @@
-import 'dart:async';
-
 import 'package:aac/src/features/symbols/model/communication_symbol.dart';
+import 'package:aac/src/features/symbols/ui/symbol_image.dart';
 import 'package:aac/src/shared/isar_provider.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:isar/isar.dart';
 
-// borrowed from https://twitter.com/remi_rousselet/status/1493522569005305858
-extension RefDebounceExtension on Ref {
-  Future<void> debounce(Duration duration) {
-    final completer = Completer<void>();
-    final timer = Timer(duration, () {
-      if (!completer.isCompleted) {
-        completer.complete();
-      }
-    });
-
-    onDispose(() {
-      timer.cancel();
-      if (!completer.isCompleted) {
-        completer.completeError(StateError("cancelled"));
-      }
-    });
-
-    return completer.future;
-  }
-}
-
 final searchedSymbolProvider =
     FutureProvider<List<CommunicationSymbol>>((ref) async {
   final isar = ref.watch(isarPod);
   final query = ref.watch(queryProvider);
-  await ref.debounce(const Duration(seconds: 2));
+
   return isar.communicationSymbols
       .where()
       .wordsElementStartsWith(query)
@@ -62,7 +40,7 @@ class SymbolSearchScreen extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    final results = ref.watch(searchedSymbolProvider);
+    final results = ref.watch(searchedSymbolProvider).valueOrNull;
     final theme = Theme.of(context);
     final appBarTheme = theme.copyWith(
       appBarTheme: AppBarTheme(
@@ -95,23 +73,18 @@ class SymbolSearchScreen extends ConsumerWidget {
               ),
               actions: null,
             ),
-            body: results.when(
-                error: (error, _) =>
-                    const Center(child: Text("Something went wrong 🙁")),
-                loading: () => const Center(
-                      child: CircularProgressIndicator(),
-                    ),
-                data: (data) {
-                  if (data.isEmpty) {
-                    return const Center(child: Text("Jak pusto"));
-                  }
-
-                  return ListView.builder(
+            body: results == null || results.isEmpty
+                ? const Center(child: Text("Jak pusto"))
+                : ListView.builder(
+                    itemCount: results.length,
                     itemBuilder: (context, index) {
-                      return null;
+                      final item = results[index];
+                      return ListTile(
+                        leading: SymbolImage(item.imagePath),
+                        title: Text(item.label),
+                      );
                     },
-                  );
-                })));
+                  )));
   }
 }
 
