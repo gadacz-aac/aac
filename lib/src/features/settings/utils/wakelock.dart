1bedb373203f5d6323de4f867aa6c3eee335dc43
Piotr Smoliński <piotreksmolinski04@gmail.com>
2025-02-16 14:00:27 +0100

    chore: temporary remove wakelock

--- lib/src/features/settings/utils/wakelock.dart
+++ lib/src/features/settings/utils/wakelock.dart
@@ -1,5 +1,3 @@
-import 'package:wakelock_plus/wakelock_plus.dart';
-
 import 'package:aac/src/features/settings/settings_manager.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
@@ -7,10 +5,10 @@ void startWakelock(WidgetRef ref) async {
   final isEnabled =
       await ref.watch(settingsManagerProvider).getValue("wakelock");
   if (isEnabled == null || !isEnabled) return;
-  WakelockPlus.enable();
+  // WakelockPlus.enable();
 }
 
 void stopWakelock() {
-  WakelockPlus.disable();
+  // WakelockPlus.disable();
   return;
 }
