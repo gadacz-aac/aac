001facfbfe2ba57f0914c6512c2be942a8b9d712
Piotr Smoliński <piotreksmolinski04@gmail.com>
2025-07-04 00:37:44 +0200

    chore: re add wakelock

--- lib/src/features/settings/utils/wakelock.dart
+++ lib/src/features/settings/utils/wakelock.dart
@@ -1,15 +1,16 @@
 import 'package:aac/src/features/settings/settings_manager.dart';
 import 'package:aac/src/features/settings/ui/settings_screen.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:wakelock_plus/wakelock_plus.dart';
 
 void startWakelock(WidgetRef ref) async {
   final isEnabled =
       ref.watch(settingsManagerProvider).getValue(SettingKey.wakelock);
   if (isEnabled == null || !isEnabled) return;
-  // WakelockPlus.enable();
+  WakelockPlus.enable();
 }
 
 void stopWakelock() {
-  // WakelockPlus.disable();
+  WakelockPlus.disable();
   return;
 }
