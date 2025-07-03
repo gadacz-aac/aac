12c77ad85bbe7b49e71360e7e2e76ef05cee9901
Piotr Smoliński <piotreksmolinski04@gmail.com>
2025-07-04 00:37:44 +0200

    chore: re add wakelock

--- lib/src/features/settings/utils/protective_mode.dart
+++ lib/src/features/settings/utils/protective_mode.dart
@@ -1,11 +1,13 @@
 import 'dart:io';
 
 import 'package:aac/src/features/settings/settings_manager.dart';
+import 'package:aac/src/features/settings/ui/settings_screen.dart';
 import 'package:flutter/foundation.dart';
 import 'package:kiosk_mode/kiosk_mode.dart';
 
 Future<void> startProtectiveModeIfEnabled(ref) async {
-  final isEnabled = await ref.read(settingsManagerProvider).getValue("kiosk");
+  final isEnabled =
+      ref.read(settingsManagerProvider).getValue(SettingKey.kiosk);
 
   if (kIsWeb) return;
   if (!Platform.isAndroid) return;
