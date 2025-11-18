97c8bb5226a7e306f3bf79fcbe11b60afe4df152
Piotr <piotreksmolinski04@gmail.com>
2025-11-18 13:08:03 +0100

    feat: proper error handling for image download

--- lib/src/shared/utils/try_download_image.dart
+++ lib/src/shared/utils/try_download_image.dart
@@ -7,9 +7,14 @@ import 'package:path_provider/path_provider.dart';
 /// Download an image from [uri] and save it to temporary directory, it's your resposibility to move it somewhere safe
 /// Retuns File or an error message if something went wrong, if there was an error file is null and vice versa
 Future<(File?, String?)> tryDownloadImage(Uri uri) async {
-  // TODO this can i throw for various reasons, one might be url that can't be resolved because Łukasz broke DNS
-  final request = await HttpClient().getUrl(uri);
-  final response = await request.close();
+  HttpClientResponse response;
+
+  try {
+    final request = await HttpClient().getUrl(uri);
+    response = await request.close();
+  } catch (e) {
+    return (null, "Nie udało się pobrać obrazka");
+  }
 
   final contentType = response.headers.contentType;
 
@@ -24,8 +29,11 @@ Future<(File?, String?)> tryDownloadImage(Uri uri) async {
     file = File('${tempDir.path}/$fileName.${contentType.subType}');
   } while (file.existsSync());
 
-  // TODO handle failed writes
-  await response.pipe(file.openWrite());
+  try {
+    await response.pipe(file.openWrite());
+  } catch (e) {
+    return (null, "Nie udało się zapisać obrazka");
+  }
 
   return (file, null);
 }
